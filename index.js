cbcec136314aa696a9d9181b32cc7de35ac440a8
Brendon Pierson <BrendonPierson@gmail.com>
2016-01-06 09:33:44 -0500

    Added thank you get route

--- index.js
+++ index.js
@@ -5,20 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/build'));
 
-// app.get('/', function(request, response) {
-//   response.send('<p>some html</p>');
-// });
+app.get('/thankyou', function(request, response) {
+  response.send('<p>Thankyou</p>');
+});
 
 app.post('/thankyou', function (req, res) {
   res.send('POST request to the homepage');
 });
-
-// var server = app.listen(3000, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   console.log('Example app listening at http://%s:%s', host, port);
-// });
 
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
