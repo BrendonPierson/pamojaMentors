8bdd9473197f4d9195bea58d8be960a55059e7fc
Brendon Pierson <BrendonPierson@gmail.com>
2016-01-06 09:11:52 -0500

    Fixed path in index.js build dir

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('build'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/build'));
 
 app.post('/thankyou', function (req, res) {
   res.send('POST request to the homepage');
 });
 
-var server = app.listen(3000, function () {
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
+
+var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Node app is running on port', app.get('port'));
 });
