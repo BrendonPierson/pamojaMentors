8d06ec2903466c44176ac2866433e74022890361
Brendon Pierson <BrendonPierson@gmail.com>
2016-01-07 09:55:35 -0500

    added post handling

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var querystring = require('querystring');
+var fb = require('firebase');
 
 var pp_hostname = "https://www.sandbox.paypal.com"; // Change to www.sandbox.paypal.com to test against sandbox
 
+var ref = new Firebase('https://pamoja.firebaseio.com/');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/build'));
 
 app.get('/thankyou', function (req, res) {
-  console.log('REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ  REQ');
-  console.log(req);
   var tx = req.query.tx;
-  console.log("TX TX TX TX TX TX TX" + tx);
   ppHandshake(tx);
+});
 
+app.post('/thankyou', function (req, res) {
+  var amount = Number(req.query.amt.split('%2e').join('.'));
+
+  ref.child('participants').child(req.query.item_number).child('moneyRaised').transaction(function(money) {
+    return money + amount;
+  });
+
+  ref.child('donations').child(req.query.tx).set({
+    tx: req.query.tx,
+    amt: amount,
+    participant: req.query.item_number,
+    sig: req.query.sig,
+    date: Date.now()
+  });
+
+  res.send("</h1>Thank you for your donation of ", amount);
 });
 
 var server = app.listen(app.get('port'), function() {
@@ -60,7 +75,7 @@ function ppHandshake(tx) {
       console.log('No more data in response.')
     })
   });
-  
+
   req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
